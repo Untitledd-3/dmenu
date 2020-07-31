05202411023ac4f9841ce49851b73ce6d43082b0
Untitledd-3 <52161745+Untitledd-3@users.noreply.github.com>
2020-07-31 13:32:33 +0100

    changes dont know what

--- config.h
+++ config.h
@@ -10,12 +10,7 @@ static const char *fonts[] = {
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+#include "/home/ed/.cache/wal/colors-wal-dmenu.h"
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
 	[SchemeNorm] = { fgalpha, bgalpha },
